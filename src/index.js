3900b1eb19f3768e3a676416443f8074eb72da00
MitziYolotzin <ultravisuell@gmail.com>
2019-06-01 17:05:34 -0500

    functions for display style for onClick sections views

--- src/index.js
+++ src/index.js
@@ -27,3 +27,71 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block"; 
   dots[slideIndex-1].className += " active";
 }
+
+
+
+
+///
+
+  
+ 
+
+
+//
+function myAbout() {
+  var project = document.getElementById('project');
+  var about = document.getElementById('aboutme-main');
+  var skills = document.getElementById('skills');
+  var side = document.getElementById('side');
+  
+  
+  if (about.style.display === 'block' && side.style.display === 'block') {
+    
+    project.style.display = 'none';
+    skills.style.display = 'none';
+  } else {
+    about.style.display = 'block';
+    side.style.display = 'block';
+    project.style.display = 'none';
+    skills.style.display = 'none';
+  }
+}
+
+//
+function myProjects() {
+  var project = document.getElementById('project');
+  var about = document.getElementById('aboutme-main');
+  var skills = document.getElementById('skills');
+  var side = document.getElementById('side');
+  if (project.style.display === 'block') {
+    
+    about.style.display = 'none';
+    skills.style.display = 'none';
+    side.style.display = 'none';
+  } else {
+    project.style.display = 'block';
+    about.style.display = 'none';
+    skills.style.display = 'none';
+    side.style.display = 'none';
+  }
+}
+
+//
+function mySkills() {
+  var project = document.getElementById('project');
+  var about = document.getElementById('aboutme-main');
+  var skills = document.getElementById('skills');
+  var side = document.getElementById('side');
+  if (skills.style.display === 'block') {
+    
+    about.style.display = 'none';
+    project.style.display = 'none';
+    side.style.display = 'none';
+  } else {
+    skills.style.display = 'block';
+    about.style.display = 'none';
+    project.style.display = 'none';
+    side.style.display = 'none';
+  }
+
+}
